531128672cc2957cbb0baef0d7b1fbfe681c96cc
Tifani Warnita <tiffayumuyuka@gmail.com>
2016-11-02 01:00:16 +0700

    Implement register request

--- NandeNyaa Server/src/NandeNyaaServer.java
+++ NandeNyaa Server/src/NandeNyaaServer.java
@@ -6,16 +6,17 @@ import org.json.simple.parser.JSONParser;
  * Created by Tifani on 10/28/2016.
  */
 public class NandeNyaaServer {
-    private static final String SERVER_ADDRESS = "localhost";
-    private static final String SERVER_QUEUE_NAME = "rpc_nandenyaa";
     private Connection connection = null;
     private Channel channel;
     private QueueingConsumer consumer;
 
     public NandeNyaaServer() {
+        // Prepare database
+        DatabaseHelper.openConnection();
+
         // Create new connection
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost(SERVER_ADDRESS);
+        factory.setHost(Constants.SERVER_ADDRESS);
         try {
             // Create a connection and channel
             connection = factory.newConnection();
@@ -26,19 +27,19 @@ public class NandeNyaaServer {
             // Non-durable (won't survive a server restart)
             // Exclusive (other channels cannot connect to the same queue)
             // Not auto-deleted once it’s no longer being used
-            channel.queueDeclare(SERVER_QUEUE_NAME, false, false, true, null);
+            channel.queueDeclare(Constants.SERVER_QUEUERE_NAME, false, false, true, null);
 
             // Accept only one un-ack-ed message at a time
             channel.basicQos(1);
 
             // Create the consumer
             consumer = new QueueingConsumer(channel);
-            channel.basicConsume(SERVER_QUEUE_NAME, false, consumer);
+            channel.basicConsume(Constants.SERVER_QUEUERE_NAME, false, consumer);
 
             System.out.println(" [x] Awaiting RPC requests");
 
             while (true) {
-                String response = null;
+                JSONObject response = null;
                 QueueingConsumer.Delivery delivery = consumer.nextDelivery();
 
                 AMQP.BasicProperties props = delivery.getProperties();
@@ -48,18 +49,18 @@ public class NandeNyaaServer {
                         .build();
 
                 try {
-                    JSONObject message = (JSONObject) (new JSONParser())
+                    JSONObject request = (JSONObject) (new JSONParser())
                             .parse(new String(delivery.getBody(), "UTF-8"));
 
-                    System.out.println(" [.] Get message: " + message.toJSONString());
-                    response = "" + "Nyaw";
+                    System.out.println(" [.] Get message: " + request.toJSONString());
+                    response = processRequest(request);
                 } catch (Exception e) {
                     System.out.println(" [.] " + e.toString());
-                    response = "";
+                    response = ResponseBuilder.buildUnknownErrorMessage("Unknown error");
 
                 }
                 finally {
-                    channel.basicPublish("", props.getReplyTo(), replyProps, response.getBytes("UTF-8"));
+                    channel.basicPublish("", props.getReplyTo(), replyProps, response.toJSONString().getBytes("UTF-8"));
                     channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
                 }
             }
@@ -75,6 +76,21 @@ public class NandeNyaaServer {
                 }
             }
         }
+    }
+
+    public JSONObject processRequest(JSONObject request) {
+        JSONObject response = null;
+        String type = String.valueOf(request.get(Constants.REQUEST_TYPE));
+        System.out.println(request.toJSONString());
+        System.out.println("Type: " + type);
+        // Register
+        if(type.equals(Constants.REGISTER)) {
+            response = DatabaseHelper.register(
+                    String.valueOf(request.get(Constants.USERNAME)),
+                    String.valueOf(request.get(Constants.PASSWORD)));
+        }
+
+        return response;
     }
 
     public static void main(String[] argv) {
